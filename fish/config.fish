0d83205e85b7034b1c0f33def58ddf838d665ea9
Owais Lone <loneowais@gmail.com>
2013-10-17 20:27:52 +0530

    Added plugin to store custom functions; Added functions to lock and unlock dpkg package versions

--- fish/config.fish
+++ fish/config.fish
@@ -2,12 +2,12 @@
 set fish_path $HOME/.oh-my-fish
 
 # Theme
-set fish_theme robbyrussell
+set fish_theme agnoster
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
 # Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
 # Example format: set fish_plugins autojump bundler
-set fish_plugins virtualfish paths
+set fish_plugins virtualfish paths aliases
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
